ceb59de5db084a765183babb721f8fc48231dbe0
Nir Zagdanski <zeged@yahoo.com>
2018-11-16 07:49:24 +0200

    first commit

---
+++ touch1/touch1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using AiForms.Effects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace touch1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+            var recognizer = AddTouch.GetRecognizer(container);
+
+            recognizer.TouchBegin += (sender, e) => {
+                Debug.WriteLine("TouchBegin");
+            };
+
+            recognizer.TouchMove += (sender, e) => {
+                Debug.WriteLine("TouchMove");
+                Debug.WriteLine($"X: {e.Location.X} Y:{e.Location.Y}");
+            };
+
+            recognizer.TouchEnd += (sender, e) => {
+                Debug.WriteLine("TouchEnd");
+            };
+
+            recognizer.TouchCancel += (sender, e) => {
+                Debug.WriteLine("TouchCancel");
+            };
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
